674df39b0d23686aac75bb6c028894ba29bb3fac
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-11-11 06:54:39 +0900

    Style: 드롭다운 메뉴 추가

--- src/javascript/pages/postUpload.js
+++ src/javascript/pages/postUpload.js
@@ -1,15 +1,20 @@
+import DropDown from '../common/dropDown.js';
+
 class PostUpload {
     render() {
-        const container = document.createElement("div");
-        const element = document.createElement("h1");
-        element.innerText = "PostUpload - 게시글 편집";
+        const container = document.createElement('div');
+        const element = document.createElement('h1');
+        element.innerText = 'PostUpload - 게시글 편집';
+        // hi
+        const anchor = document.createElement('a');
+        anchor.href = '/';
+        anchor.innerText = '테스트 페이지로 이동';
 
-        const anchor = document.createElement("a");
-        anchor.href = "/";
-        anchor.innerText = "테스트 페이지로 이동";
+        const dropDown = new DropDown();
 
         container.appendChild(element);
         container.appendChild(anchor);
+        container.appendChild(dropDown.render());
 
         return container;
     }
