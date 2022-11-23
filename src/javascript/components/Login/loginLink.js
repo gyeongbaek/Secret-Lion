4f7e1aa2d09b7f17774e8f2f79a415ef587334bc
kngsujng <kngsujng@naver.com>
2022-11-23 17:11:04 +0900

    Refact: 로그인 페이지 코드 리펙토링

--- src/javascript/components/Login/loginLink.js
+++ src/javascript/components/Login/loginLink.js
@@ -3,16 +3,16 @@ import Component from "../../core/Component.js";
 class LoginLink extends Component {
     render() {
         const linkCont = document.createElement('div');
-        linkCont.classList.add('loginPage_div_linkCont');
+        linkCont.setAttribute('class', 'loginPage_div_linkCont');
 
         const searchLink = document.createElement('a');
-        searchLink.setAttribute('href', '#');
-        searchLink.classList.add('loginPage_a_search');
+        searchLink.href = '#';
+        searchLink.setAttribute('class', 'loginPage_a_search');
         searchLink.textContent = '아이디·비밀번호 찾기';
 
         const signUpLink = document.createElement('a');
-        signUpLink.setAttribute('href', 'src/javascript/pages/signupPage.js');
-        signUpLink.classList.add('loginPage_a_signUp');
+        signUpLink.href = '/signup';
+        signUpLink.setAttribute('class', 'loginPage_a_signUp');
         signUpLink.textContent = '회원가입';
 
         linkCont.appendChild(searchLink);
