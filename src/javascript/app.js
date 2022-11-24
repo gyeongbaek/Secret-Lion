6670c51ffbb599cee6152670e841a57342a2be6e
kngsujng <kngsujng@naver.com>
2022-11-25 03:41:22 +0900

    Feat: 회원가입 기능 구현

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -19,7 +19,7 @@ export default class App {
         const { el } = this.props;
 
         const router = new Router({
-            '/': TestPage,
+            '/': SignupPage,
             '/start': StartPage,
             '/login': LoginPage,
             '/signup': SignupPage,
