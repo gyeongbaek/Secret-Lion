d44ef680f643b7d6e53f208fb7439781a6c206ec
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-12-05 09:21:59 +0900

    Feat: 메인페이지에서 게시물 클릭시 해당 게시물 연결

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,35 +1,35 @@
 import {
-    StartPage,
-    LoginPage,
-    MainPage,
-    PostDetailPage,
-    PostUploadpage,
-    SignupPage,
-    UserEdit,
-    UserInfo,
-    TestPage,
+  StartPage,
+  LoginPage,
+  MainPage,
+  PostDetailPage,
+  PostUploadpage,
+  SignupPage,
+  UserEdit,
+  UserInfo,
+  TestPage,
 } from './pages/index.js';
 import { Router } from './utils/index.js';
 
 export default class App {
-    constructor(props) {
-        this.props = props;
-        this.token = localStorage.getItem('token');
-    }
-    async setup() {
-        const { el } = this.props;
-        console.log(this.token);
-        const router = new Router({
-            '/': PostUploadpage,
-            '/start': StartPage,
-            '/login': LoginPage,
-            '/signup': SignupPage,
-            '/main': MainPage,
-            '/post/:id': PostDetailPage,
-            '/upload': PostUploadpage,
-            '/user': UserInfo,
-            '/setting': UserEdit,
-        });
-        router.init(el);
-    }
+  constructor(props) {
+    this.props = props;
+    this.token = localStorage.getItem('token');
+  }
+  async setup() {
+    const { el } = this.props;
+    console.log(this.token);
+    const router = new Router({
+      '/': MainPage,
+      '/start': StartPage,
+      '/login': LoginPage,
+      '/signup': SignupPage,
+      '/main': MainPage,
+      '/post/:id': PostDetailPage,
+      '/upload': PostUploadpage,
+      '/user': UserInfo,
+      '/setting': UserEdit,
+    });
+    router.init(el);
+  }
 }
